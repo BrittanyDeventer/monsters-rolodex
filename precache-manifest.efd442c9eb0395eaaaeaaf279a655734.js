c9dedaabd70df26fab301299aa2015a3c3b18fc5
BrittanyDeventer <39882665+BrittanyDeventer@users.noreply.github.com>
2019-10-23 09:02:28 -0700

    Updates

---
+++ precache-manifest.efd442c9eb0395eaaaeaaf279a655734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c0e05a089d549caa95b878d76d2b59",
+    "url": "/BrittanyDeventer/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "70c13c95e67725d0d0ca",
+    "url": "/BrittanyDeventer/monsters-rolodex/static/css/main.697b03e1.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/BrittanyDeventer/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "70c13c95e67725d0d0ca",
+    "url": "/BrittanyDeventer/monsters-rolodex/static/js/main.a57af7f8.chunk.js"
+  },
+  {
+    "revision": "88d88d78c623699af08d",
+    "url": "/BrittanyDeventer/monsters-rolodex/static/js/runtime-main.0034f2e2.js"
+  }
+]);
